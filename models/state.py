dad1c5e56a3dae61d4562306f525509e90e189fd
steven-cruz <stevencruz.300008@gmail.com>
2020-02-19 22:54:49 +0000

    state

--- models/state.py
+++ models/state.py
@@ -2,9 +2,10 @@
 """ making state inherence from basemodel """
 
 
+import models
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-
+    """ public Class attributes """
     name = ""
