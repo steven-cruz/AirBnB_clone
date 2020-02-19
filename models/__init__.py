8b4a506a6ab6aae48213c80296da568d67da54fc
steven-cruz <stevencruz.300008@gmail.com>
2020-02-19 19:08:50 +0000

    #

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""Create a unique FileStorage instance"""
+
+
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 storage = FileStorage()
 storage.reload()
